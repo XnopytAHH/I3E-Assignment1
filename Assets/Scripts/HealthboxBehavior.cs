c70e1a8dd72165dfd256108cfd3ce29a5af655af
XnopytAHH <s10269017@connect.np.edu.sg>
2025-06-10 00:03:24 +0800

    Healthbox spawning
    Fixed launching angles for health pickups

--- Assets/Scripts/HealthboxBehavior.cs
+++ Assets/Scripts/HealthboxBehavior.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HealthboxBehavior : MonoBehaviour
 {
@@ -14,14 +18,22 @@ public class HealthboxBehavior : MonoBehaviour
     [SerializeField]
     float distance = 2.0f; // Distance that health pickups get fired from the healthbox
                            // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    float spawnDelay = 0.5f; // Delay between spawning health pickups
+    List<int> rotations = new List<int>(); // List to store random rotations for health pickups
     
     public void Interact()
     {
         if (canInteract)
         {
+            
             canInteract = false; // Prevent further interactions until the coroutine completes
             float spawnCount = 0; // Counter to track the number of health pickups spawned
-            
+            for (int i = 0; i < healthAmount; i++)
+            {
+                rotations.Add(i*(360/healthAmount)); // Add random rotations to the list
+            }
+            Debug.Log(rotations);
             StartCoroutine(SpawnHealth(spawnCount)); // Start the coroutine to spawn health pickups
         }
 
@@ -31,13 +43,14 @@ public class HealthboxBehavior : MonoBehaviour
     {
         if (spawnCount < healthAmount)
         {
-
-            spawnPoint.rotation = Quaternion.Euler(0, Random.Range(0, healthAmount), 30); // Randomize the rotation of the health drop
+            int spawnAngle = UnityEngine.Random.Range(0, rotations.Count);
+            spawnPoint.rotation = Quaternion.Euler(-70, rotations[spawnAngle], 0); // Set the rotation for the health pickup
+            rotations.RemoveAt(spawnAngle); // Remove the used rotation to avoid duplicates
             GameObject healthDrop = Instantiate(health, spawnPoint.position, health.transform.rotation);
             Vector3 fireForce = spawnPoint.forward * distance;
             healthDrop.GetComponent<Rigidbody>().AddForce(fireForce);
             spawnCount++; // Increment the spawn count
-            yield return new WaitForSeconds(0.3f); // Wait for 0.5 seconds before spawning health
+            yield return new WaitForSeconds(spawnDelay); // Wait for a delay before spawning health
             StartCoroutine(SpawnHealth(spawnCount)); // Continue spawning health pickups until the limit is reached
         }
     }
